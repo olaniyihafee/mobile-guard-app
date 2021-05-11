a9d71a5f8a23c1eeeb8932bdfe7abd7a4bfc79b4
olaniyihafee <79503753+olaniyihafee@users.noreply.github.com>
2021-05-11 21:57:56 +0100

    another git commit

--- server2-typescript/api/src/config/db.ts
+++ server2-typescript/api/src/config/db.ts
@@ -9,11 +9,13 @@ const {
   MONGO_DATABASE = 'auth'
 } = process.env
 
+export const MONGO_URI = `mongodb+srv://admin:8TKZZEKCtooCv3okgit@cluster0.1cuub.mongodb.net/auth?retryWrites=true&w=majority`
+//export const MONGO_URI = `mongodb+srv://<username>:<password>@cluster0.1cuub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 //export const MONGO_URI = `mongodb://root:secret@localhost:27017/auth?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false`
 
-export const MONGO_URI = `mongodb://${MONGO_ROOTNAME}:${
+/*export const MONGO_URI = `mongodb://${MONGO_ROOTNAME}:${
   encodeURIComponent(MONGO_PASSWORD)
-}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}?${MONGO_DATABASE}Source=${MONGO_USERNAME}`
+}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}?${MONGO_DATABASE}Source=${MONGO_USERNAME}`*/
 
 export const MONGO_OPTIONS: ConnectionOptions = {
   useNewUrlParser: true,
