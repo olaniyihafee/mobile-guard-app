62c6d2d78e58be700fc4b43f173ecd2403248a62
olaniyihafee <79503753+olaniyihafee@users.noreply.github.com>
2021-05-17 22:04:09 +0100

    changed the mongo uri string to be able to connect with that of heroku

--- server2-typescript/api/dist/config/db.js
+++ server2-typescript/api/dist/config/db.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MONGO_OPTIONS = exports.MONGO_URI = void 0;
 const { MONGO_ROOTNAME = 'root', MONGO_USERNAME = 'admin', MONGO_PASSWORD = 'secret', MONGO_HOST = 'localhost', MONGO_PORT = 27017, MONGO_DATABASE = 'auth' } = process.env;
-exports.MONGO_URI = `mongodb+srv://admin:8TKZZEKCtooCv3okgit@cluster0.1cuub.mongodb.net/auth?retryWrites=true&w=majority`;
+const mongo_uri_from_heroku = process.env.MONGODB_URI;
+exports.MONGO_URI = mongo_uri_from_heroku;
 exports.MONGO_OPTIONS = {
     useNewUrlParser: true,
     useUnifiedTopology: true
